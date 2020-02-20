8ceba5c12759e7362c00b10b05ba039accd3abb7
Roman Kushnir <roman.kushnir@liefery.com>
2020-02-20 20:46:11 +0100

    Pass product_import_form as a parameter to the template

--- app/controllers/product_imports_controller.rb
+++ app/controllers/product_imports_controller.rb
@@ -1,19 +1,20 @@
 class ProductImportsController < ApplicationController
   def new
-    @product_import_form = ProductImportForm.new
+    form = ProductImportForm.new
+    render :new, locals: { form: form }
   end
 
   def create
-    @product_import_form = ProductImportForm.new
+    form = ProductImportForm.new
 
-    if @product_import_form.validate(params.require(:product_import))
-      ImportProductsFromCsv.new.call(@product_import_form.csv_file)
+    if form.validate(params.require(:product_import))
+      ImportProductsFromCsv.new.call(form.csv_file)
 
       flash[:notice] = t("product_imports.create.success")
       redirect_to products_path
     else
       flash.now[:alert] = t("product_imports.create.failure")
-      render :new
+      render :new, locals: { form: form }
     end
   end
 end
